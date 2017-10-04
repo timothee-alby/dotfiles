1ce1a13f16c49da9f8e946728492f126c78d8191
Tim Alby <timothee.alby@gmail.com>
2017-10-04 15:43:55 +0700

    add Plug to install script

--- install.sh
+++ install.sh
@@ -81,6 +81,9 @@ if [ ! -r ~/.oh-my-zsh ]; then
   curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 fi
 
+# install Plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Get current working directory
 CWD=$(pwd)
